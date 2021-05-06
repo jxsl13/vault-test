e88d1b5d60eadd0e6011539e69e0a97c4d15a803
Behm, John, NMD-A6 (Extern) <john.behm.extern@bertelsmann.de>
2021-05-06 09:23:33 +0200

    print text when waiting for deploymnet startup

--- k8s/scripts/remote_install_vault.sh
+++ k8s/scripts/remote_install_vault.sh
@@ -4,5 +4,7 @@ kubectl create namespace vault
 
 helm install consul hashicorp/consul --namespace vault --values values/consul-vault-values.yaml
 helm install vault  hashicorp/vault --namespace vault --values values/vault-values.yaml
+
+echo "Waiting for the deployment to startup..."
 kubectl rollout status daemonset.apps/consul-consul --timeout=60s -n vault | grep 'daemon set \"consul-consul\" successfully rolled out'
 sleep 5
